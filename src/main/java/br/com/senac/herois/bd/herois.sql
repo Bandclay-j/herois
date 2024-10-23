f07ee4a8b82268c96d91f010e6884e8928484586
Talisson-Rodrigues <talisontalison57@gmail.com>
2024-10-23 10:23:34 -0300

    as

--- src/main/java/br/com/senac/herois/bd/herois.sql
+++ src/main/java/br/com/senac/herois/bd/herois.sql
@@ -9,5 +9,11 @@ CREATE TABLE herois (
     super_poder VARCHAR(255) NOT NULL,
     fraqueza VARCHAR(255) NOT NULL,
     historia_origem TEXT NOT NULL,
-    primeira_aparicao DATE NOT NULL
+    primeira_aparicao DATE NOT NULL,
+    time_id BIGINT NOT NULL
 );
+
+CREATE TABLE time {
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+}
