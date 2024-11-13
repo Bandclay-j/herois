4549e6c4232be8c3d2ac24568dab3ac677481d06
Talisson-Rodrigues <talisontalison57@gmail.com>
2024-11-13 09:34:04 -0300

    Atualização endPoints

--- src/main/java/br/com/senac/herois/HeroisApplication.java
+++ src/main/java/br/com/senac/herois/HeroisApplication.java
@@ -2,6 +2,10 @@ package br.com.senac.herois;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HeroisApplication {
@@ -9,5 +13,19 @@ public class HeroisApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HeroisApplication.class, args);
 	}
+
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://127.0.0.1:5500") // ajuste a origem conforme necessário
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // métodos HTTP permitidos
+                        .allowedHeaders("*") // cabeçalhos permitidos
+                        .allowCredentials(true); // permite credenciais (opcional)
+            }
+        };
+    }
 
 }
--- src/main/java/br/com/senac/herois/controller/EquipeController.java
+++ src/main/java/br/com/senac/herois/controller/EquipeController.java
@@ -1,13 +1,21 @@
 package br.com.senac.herois.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.senac.herois.entity.Equipe;
 import br.com.senac.herois.repository.EquipeRepository;
+
 
 
 
@@ -35,4 +43,49 @@ public class EquipeController {
         return new ResponseEntity<>(equipeRepository.findById(id), HttpStatus.OK);
     }
     
+    @PostMapping("/equipes")
+    public ResponseEntity<?> salvaEquipe(@RequestBody Equipe entity) {
+        Equipe equipeSalva;
+        try {
+            equipeSalva = equipeRepository.save(entity);
+        } catch (Exception e) {
+            return new ResponseEntity<String>("Erro ao salvar a equipe", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<Equipe>(equipeSalva, HttpStatus.OK);
+    }
+
+    @PutMapping("equipes/{id}")
+    public ResponseEntity<?> atualizaEquipe(@PathVariable int id, @RequestBody Equipe entity) {
+        Optional<Equipe> equipeAtualizar = equipeRepository.findById(id);
+        Equipe e = null;
+        if (equipeAtualizar.isPresent()) {
+            e = equipeAtualizar.get();
+            e.setNome(entity.getNome());
+            
+            try {
+                e = equipeRepository.save(e);
+            } catch (Exception s) {
+                return new ResponseEntity<String>("Erro ao atualizar a equipe", HttpStatus.BAD_REQUEST);
+            }
+            return new ResponseEntity<Equipe>(e, HttpStatus.OK); 
+        } else {
+            return new ResponseEntity<String>("Equipe não encontrada", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/equipes/{id}")
+    public ResponseEntity<String> deletaEquipe(@PathVariable int id) {
+        Optional<Equipe> equipeExcluir = equipeRepository.findById(id);
+
+        if (equipeExcluir.isPresent()) {
+            try {
+                equipeRepository.delete(equipeExcluir.get());
+                return new ResponseEntity<>("Equipe excluída com sucesso", HttpStatus.NO_CONTENT);
+            } catch (Exception e) {
+                return new ResponseEntity<>("Erro ao excluir a equipe", HttpStatus.BAD_REQUEST);
+            }
+        } else {
+            return new ResponseEntity<>("Equipe não encontrada", HttpStatus.NOT_FOUND);
+        }
+    }
 }
